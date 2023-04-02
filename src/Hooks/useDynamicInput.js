01f190cb47816d02bf920185577eed4cd0a9af80
sarwar-asik <shasik2021@gmail.com>
2023-04-02 19:54:56 +0600

    first commit

---
+++ src/Hooks/useDynamicInput.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+// 
+//   const [handleChange, addInput, removeInputList,inputList] = useDynamicInput() ///
+
+//   {inputList.map((item, index) => {
+//             return (
+//               <div key={index} className="mb-1 sm:mb-2">
+//                 <label className="inline-block mb-1 font-medium">Skills</label>
+//                 <input
+//                   value={item} 
+//                   onChange={(e) => handleChange(e.target.value, index)}
+//                 />
+//                 {inputList.length > 1 && (
+//                   <p
+//                     className="bg-red-300 p-2 "
+//                     onClick={() => removeInputList(index)}
+//                   >
+//                     Remove
+//                   </p>
+//                 )}
+//               </div>
+//             );
+//           })}
+//         </section>
+//         <p className="bg-slate-200 p-3 " onClick={() => addInput()}>
+//           Add Skills
+//         </p>
+
+
+const useDynamicInput = () => {
+  const [inputList, setInputList] = useState([]);
+
+  const handleChange = (e, index) => {
+    const inputData = [...inputList];
+    inputData[index] = e;
+
+    setInputList(inputData);
+  };
+
+  const addInput = () => {
+    const addData = [...inputList, []];
+    setInputList(addData);
+  };
+
+  const removeInputList = (i) => {
+    const list = [...inputList];
+    list.splice(i, 1);
+    setInputList(list);
+  };
+
+  return [handleChange, addInput, removeInputList, inputList];
+};
+
+export default useDynamicInput;
